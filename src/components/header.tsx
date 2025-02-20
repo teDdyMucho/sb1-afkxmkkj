c11acbad71e21abb3edbe5a0f0d7a35b8e27ca88
teDdyMucho <tapangjoseangelo@gmail.com>
2025-02-20 16:01:08 +0800

    U-Gameinfo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -70,7 +70,7 @@ export function Header() {
                     className="flex items-center space-x-2 text-blue-600 hover:bg-blue-50 hover:text-blue-700"
                   >
                     <User className="h-4 w-4" />
-                    <span>User Panel</span>
+                    <span>Game Info</span>
                   </Button>
                   <Button
                     onClick={() => navigateToPanel('game')}
